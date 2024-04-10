8a344d444c9f7a4e130460791277e59bf1b68131
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-04-10 16:03:58 +0200

    setup dark/light mood and finished the header

---
+++ EduNexProject/src/app/Components/header/header.component.ts
@@ -0,0 +1,57 @@
+import { Component, HostBinding, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSidenav } from '@angular/material/sidenav';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  theme = new FormControl(false);
+  @HostBinding('class') className = '';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  darkClass = 'theme-dark';
+  lightClass = 'theme-light';
+  showFiller = false;
+  isShowing: boolean = false;
+
+  toggleRightSidenav() {
+    this.isShowing = !this.isShowing;
+  }
+
+  // callMethods() {
+  //   this.toggleSidenav();
+  // }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.applyTheme(this.theme.value);
+    this.theme.valueChanges.subscribe((currentTheme) => {
+      this.applyTheme(currentTheme);
+    });
+  }
+
+  private applyTheme(currentTheme: boolean | null): void {
+    if (currentTheme === null) { // Handle null value
+      currentTheme = false; // Set default value to false
+    }
+
+    this.className = currentTheme ? this.darkClass : this.lightClass;
+    const bodyElement = document.getElementsByTagName('body')[0];
+
+    if (currentTheme) {
+      bodyElement.classList.add(this.darkClass);
+      bodyElement.classList.remove(this.lightClass);
+    } else {
+      bodyElement.classList.add(this.lightClass);
+      bodyElement.classList.remove(this.darkClass);
+    }
+  }
+
+}
+
+
--- EduNexProject/src/app/app.module.ts
+++ EduNexProject/src/app/app.module.ts
@@ -1,25 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
 import { LoginComponent } from './Components/login/login.component';
-import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './Components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { OverlayContainer } from '@angular/cdk/overlay'; // Remove this line
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './Components/footer/footer.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    OverlayContainer, // Provide OverlayContainer here
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
