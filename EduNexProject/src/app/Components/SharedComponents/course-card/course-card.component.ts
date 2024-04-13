324ef59c09748d002add54d65118cdcd057e8575
Mohamed Alaa <mohamed.alaa.elhefny@gmail.com>
2024-04-13 05:04:43 +0200

    change the wave section style

--- EduNexProject/src/app/Components/SharedComponents/course-card/course-card.component.ts
+++ EduNexProject/src/app/Components/SharedComponents/course-card/course-card.component.ts
@@ -15,10 +15,10 @@ export class CourseCardComponent {
       const cardRect = card.getBoundingClientRect();
       const mouseX = event.clientX - cardRect.left;
       const mouseY = event.clientY - cardRect.top;
-      const shadowX = (mouseX - cardRect.width / 2) / 25;
-      const shadowY = (mouseY - cardRect.height / 2) / 25;
+      const shadowX = (mouseX - cardRect.width / 2) / 10;
+      const shadowY = (mouseY - cardRect.height / 2) / 10;
       card.style.transform = `rotateX(${-shadowY}deg) rotateY(${shadowX}deg)`;
-      card.style.boxShadow = `${shadowX}px ${shadowY}px 0px 1px var(--border), var(--border) 0px 0px 0px 1px`;
+      card.style.boxShadow = `${shadowX}px ${shadowY}px 10px 5px var(--border), var(--border) 0px 0px 0px 1px`;
     }
   }
 
